5ea1173b5b424946671e1295bc0388874300de99
Guilherme Calixto Campos <gcalixtocampos16@gmail.com>
2026-03-14 11:41:31 -0300

    altera escrita do boas vindas

--- src/components/BoasVindas/index.jsx
+++ src/components/BoasVindas/index.jsx
@@ -3,7 +3,7 @@ import './boas-vindas.style.css'
 export function BoasVindas({ src }) {
     return (
         <section className="boas-vindas">
-            <h1>Bem-vindo</h1>
+            <h1>Bem-vindo(a)</h1>
             <p>Faça login para acessar seu app de treinos</p>
             <img src={src} alt="gif de academia" />
         </section>
